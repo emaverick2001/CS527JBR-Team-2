60d5c3e6fcdd8c4b07af97c6521562d86ecf97c8
venkat0011 <76080326+venkat0011@users.noreply.github.com>
2026-03-30 19:04:51 -0500

    Add detect_inefficiency_patterns function
    Implement detect_inefficiency_patterns function to analyze model inefficiencies based on CSV data.

--- milestone3/milestone3.py
+++ milestone3/milestone3.py
@@ -122,8 +122,35 @@ def collect_graph_metrics(model_name: str, instance_id: str) -> dict:
     return graph_metrics_json
 
 
-def detect_inefficiency_patterns(model_name: str, instance_id: str) -> dict:
-    pass
+def detect_inefficiency_patterns(model_name, instance_id):
+    # model : instance {status, }
+    # metric can be obtained from csv
+    # iterate through the dataframe
+    # read in the dataframe based on the model name
+    # iloc the instance ID
+    # the assumption is analysis file is stored in analysis/swe-agent/analysis
+    inefficent_pattern = {}
+    file_name = f'analysis/SWE-agent/analysis/{model_name}/trajectory_metrics.csv'
+    graph_metrics = pd.read_csv(file_name)
+    status = graph_metrics.query(
+        f'instance == "{instance_id}"').iloc[0]['resolution']
+    inefficent_pattern['Status'] = status
+
+    anti_patterns = ['repeat_failed_edit',
+                     'flip_flop',
+                     'scroll_behavior',
+                     'back_and_forth_switch',
+                     'zoom_out',
+                     'abandonment']
+    patterns_present = []
+    for pattern in anti_patterns:
+        is_pattern_present = graph_metrics.query(
+            f'instance == "{instance_id}"').iloc[0][pattern]
+        if is_pattern_present:
+            patterns_present.append(pattern)
+    inefficent_pattern['Patterns'] = patterns_present
+    return inefficent_pattern
+    
 
 if __name__ == "__main__":
     # the trajectories to find steps for
